96c74f296646ac2d6a72fa5433425bd30cfe23a6
MackAttack <35879478+mackenzierae24@users.noreply.github.com>
2025-05-27 19:58:30 +1000

    Update main.py
    switched from simple ui to new ui

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ EchoLens - Main Application Entry Point
 Run with: streamlit run main.py
 """
 import streamlit as st
-from src.ui.streamlit_app_simple import run_app
+from src.ui.streamlit_app import run_app
 
 if __name__ == "__main__":
     run_app()
